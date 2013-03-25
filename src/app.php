3465e44ead09f7b8392c5790b3ec4ea05b151271
Neurolit <neurolit@gmail.com>
2013-03-25 22:51:33 +0100

    Configuration files are read in config/prod.php and not in src/app.php anymore

--- src/app.php
+++ src/app.php
@@ -1,36 +1,27 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Symfony\Component\Translation\Loader\YamlFileLoader as YamlFileTranslateLoader;
 
-$app = new Silex\Application();
+$app = new Application();
 
-if (file_exists(__DIR__."/../conf/app.yml")) {
-    $config = Yaml::parse(__DIR__."/../conf/app.yml");
-} else {
-    $config = Yaml::parse(__DIR__."/../conf/app_default.yml");
-}
-
-$app->register(new Inria\SEISM\Provider\EtherpadServiceProvider(), array(
-    'etherpad.protocol'     => $config['etherpad']['protocol'],
-    'etherpad.server'       => $config['etherpad']['server'],
-    'etherpad.port'         => $config['etherpad']['port'],
-    'etherpad.api_key'      => $config['etherpad']['api_key'],
-    'etherpad.public_url'   => $config['etherpad']['public_url']
-                                                                         )) ;
+$app->register(new Inria\SEISM\Provider\EtherpadServiceProvider());
 
 // use Twig
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app->register(new TwigServiceProvider(), array(
     'twig.path'       => __DIR__.'/../templates',
+    'twig.options'    => array('cache' => __DIR__.'/../cache/twig'),
 ));
 
 // Use TranslationService
-$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+$app->register(new TranslationServiceProvider(), array(
     'locale_fallback'           => 'fr',
 ));
 
 // YAML translation files
-use Symfony\Component\Translation\Loader\YamlFileLoader as YamlFileTranslateLoader;
 $app['translator'] = $app->share($app->extend('translator',
                                               function($translator, $app){
     $translator->addLoader('yaml', new YamlFileTranslateLoader());
@@ -52,20 +43,6 @@ $app['translator'] = $app->share($app->extend('translator',
 // etherpad_log MySQL
 // CREATE DATABASE etherpad_log DEFAULT CHARACTER SET 'utf8' DEFAULT COLLATE 'utf8_bin' ;
 // CREATE TABLE pad_creation (pad_id VARCHAR(250) NOT NULL PRIMARY KEY, timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, creator_address VARCHAR(15)) ;
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options'            => array(
-        'driver'    => 'pdo_mysql',
-        'host'      => $config['mysql']['host'],
-        'dbname'    => $config['mysql']['dbname'],
-        'user'      => $config['mysql']['user'],
-        'password'  => $config['mysql']['password'],
-        'charset'   => 'utf8',
-        'driverOptions' => array(PDO::MYSQL_ATTR_INIT_COMMAND
-                                 =>
-                                 'SET NAMES utf8'),
-
-    ),
-                                                                   )) ;
+$app->register(new Silex\Provider\DoctrineServiceProvider());
 
 return $app ;
-?>
