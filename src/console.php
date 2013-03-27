736f3f0789ac0c0b03a8c2341440b4178ad4b30f
Neurolit <neurolit@gmail.com>
2013-03-27 17:58:31 +0100

    deletePad command added to console

--- src/console.php
+++ src/console.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-$console = new Application('Etherpad-Lite Frontend', 'n/a');
+$console = new Application('Etherpad-Lite Frontend', '1.2.0');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console
     ->register('listAllPads')
@@ -29,6 +29,34 @@ $console
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
        $padID = $input->getArgument('padID');
        $output->writeln($app['etherpad']->getText($padID));
+    })
+;
+
+$console
+    ->register('deletePad')
+    ->setDefinition(array(
+        new InputArgument('padID', InputArgument::REQUIRED, 'Pad ID')
+    ))
+    ->setDescription('Delete a pad from etherpad-lite')
+    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app, $console) {
+       $dialog = $console->getHelperSet()->get('dialog');
+       $padID = $input->getArgument('padID');
+       if (!$dialog->askConfirmation(
+               $output,
+               '<question>Are you sure to delete the pad '.$padID.' (y/N)? </question>',
+               false
+          )) {
+          return;
+       }
+       if (!$dialog->askConfirmation(
+               $output,
+               '<question>Are you REALLY sure to delete the pad '.$padID.' (y/N)? </question>',
+               false
+          )) {
+          return;
+       }
+       $app['etherpad']->deletePad($padID);
+       $output->writeln("$padID deleted!");
     })
 ;
 
